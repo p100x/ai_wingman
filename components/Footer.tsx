0ff0c34f0bd73fb3bc08bb8880a5f4cd26305240
p100x <164509272+p100x@users.noreply.github.com>
2024-03-22 16:16:15 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,16 +6,16 @@ export default function Footer() {
       <div>
         Powered by{' '}
         <a
-          href="https://mistral.ai/news/mixtral-of-experts"
+          href="#"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
         >
-          Mixtral{' '}
+          AI Wingman{' '}
         </a>
-        and{' '}
+        {' '}
         <a
-          href="https://platform.openai.com/docs/models"
+          href="#"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
@@ -25,7 +25,7 @@ export default function Footer() {
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://twitter.com/nutlope"
+          href="#"
           className="group"
           aria-label="TaxPal on Twitter"
         >
@@ -37,7 +37,7 @@ export default function Footer() {
           </svg>
         </Link>
         <Link
-          href="https://github.com/Nutlope/twitterbio"
+          href="#"
           className="group"
           aria-label="TaxPal on GitHub"
         >
