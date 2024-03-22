5dcc8ff9de93d1022fa9d085513ca88c9e860f81
p100x <164509272+p100x@users.noreply.github.com>
2024-03-22 22:40:48 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
           className="sm:w-9 sm:h-9 w-8 h-8"
         />
         <h1 className="sm:text-3xl text-2xl font-bold ml-2 tracking-tight">
-          RizzoMat
+          Rizz Sprüche - Mit künstlicher Intelligenz zum King der Dating-Apps | RizzoMat
         </h1>
       </Link>
 
